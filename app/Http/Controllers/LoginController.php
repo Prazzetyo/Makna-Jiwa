c7cc05e6be28fc05a501e133b8d393a9654fd50e
Muhammad Dwi Prasetyo <prasetyomuhammaddwi5@gmail.com>
2021-12-20 21:36:24 +0700

    Update LoginController.php
    Change redirect ke categories

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -22,7 +22,7 @@ class LoginController extends Controller
    
         $credentials = $request->only('email', 'password');
         if (Auth::attempt($credentials)) {
-            return view('admin');
+            return redirect()->intended('categories');
         }
   
         return back()->with('loginError', 'Login Failed! Check your username or password');

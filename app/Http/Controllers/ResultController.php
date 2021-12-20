a85ac1af5d66794f960ec7ef3f95c0b8e11f932f
Muhammad Dwi Prasetyo <prasetyomuhammaddwi5@gmail.com>
2021-12-20 21:39:20 +0700

    Update ResultController.php
    Change ResultController agar saat menampilkan di view lebih dinamis lagi pada foreach

--- app/Http/Controllers/ResultController.php
+++ app/Http/Controllers/ResultController.php
@@ -2,23 +2,29 @@
 
 namespace App\Http\Controllers;
 
-use DB;
 use Illuminate\Http\Request;
+use DB;
 
 class ResultController extends Controller
 {
     public function store(Request $request)
     {
-        $answerScore = array_slice($request->all(), 2);
+        $answerScore = array_slice($request->all(), 1);
         $totalScore = 0;
         foreach ($answerScore as $score) {
             $totalScore += $score;
         }
         $totalScore /= count($answerScore);
         $totalScore *= 20;
+        
+        $body = DB::table('solusi')->where('id_kategori', $request->id_kategori)->get();
+        $solusi = DB::table('solusii')->where('id_kategori', $request->id_kategori)->get();
+        
+        return view('result', compact('totalScore', 'body', 'solusi'));
+    }
 
-        $body = DB::table('solusi')->where('id_kategori', $request->category_id)->get();
-
-        return view('result', compact('totalScore','body'));
+    public function show($result)
+    {
+        return view('result');
     }
 }
